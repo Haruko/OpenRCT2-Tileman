000d0cc7e9760a23cc8117190f84552375f29a42
Isoitiro <19146447+Haruko@users.noreply.github.com>
2024-04-27 04:25:28 -0400

    Update index.ts
    Trying to figure out how some basic stuff works it's so bad

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,35 @@
-// import {window, box, label, Colour, 
-  // button, colourPicker, dropdown, dropdownSpinner, spinner, toggle, viewport} from 'openrct2-flexui';
-// import * as FlexUI from 'openrct2-flexui';
+//   /// <reference path="../lib/openrct2.d.ts" />
 import * as FlexUI from 'openrct2-flexui';
+
+
 
 /*
   Variables
 */
+
 // Data logging
-var totalExp = 69420;
-var tilesUsed = 0;
+const PlayerData = {
+  totalExp: 69420,
+  tilesUsed: 0,
+
+  // Maps ride IDs (numbers) and historical data (Ride.totalCustomers, eventually Ride.totalProfit or something )
+  rideMap: {}
+};
 
 // Configs
-var expPerTile = 1000;
+const PluginConfig = {
+  winTitle: 'Tileman',
+  expPerTile: 1000,
+  minTiles: 2
+}
 
 
-// Don't change
-const winTitle = 'Tileman';
 
-// Globalish references
-const win = FlexUI.window({
-  title: winTitle,
+/*
+  UI construction
+*/
+const mainWindow = FlexUI.window({
+  title: PluginConfig.winTitle,
 	width: 300,
 	height: 200,
   content: [
@@ -33,7 +43,7 @@ const win = FlexUI.window({
               text: "{BLACK}Total Exp: "
             }),
             FlexUI.label({
-              text: `{BABYBLUE}${totalExp}`,
+              text: `{BABYBLUE}${context.formatString('{COMMA16}', PlayerData.totalExp)}`,
             })]
           }),
           FlexUI.horizontal({
@@ -42,7 +52,7 @@ const win = FlexUI.window({
               text: "{BLACK}Tiles Unlocked/Used: "
             }),
             FlexUI.label({
-              text: `{BABYBLUE}${computeTilesAvailable()}{BLACK}/{RED}${tilesUsed}`,
+              text: `{BABYBLUE}${computeTilesAvailable()}{BLACK}/{RED}${PlayerData.tilesUsed}`,
             })]
           })]
       })
@@ -50,32 +60,71 @@ const win = FlexUI.window({
   ]
 });
 
-function computeTilesAvailable() : Number {
-  return Math.floor(totalExp / expPerTile);
+function openWindow() {
+  closeWindow();
+  mainWindow.open();
 }
 
-function showWindow() {
-  // Close other instance of this window if it exists
+// Assumes only one instance of window open
+function closeWindow() {
   let numWindows = ui.windows;
   for(let i = 0; i < numWindows; ++i) {
     let winTest = ui.getWindow(i);
-    if (winTest.title == winTitle) {
+    if (winTest.title == PluginConfig.winTitle) {
       winTest.close();
-      break;
+      return
     }
   }
-
-  win.open();
 }
+
+
+
+/*
+  Helper Functions
+*/
+
+// Computes number of tiles earned based on total experience
+function computeTilesAvailable() : Number {
+  return Math.floor(PlayerData.totalExp / PluginConfig.expPerTile) + PluginConfig.minTiles;
+}
+
+/*
+  Data tracking
+*/
+
+function collectData() : void {
+  console.log(park.totalAdmissions);
+  console.log(map.rides[0].totalCustomers);
+  // console.log(map.rides[0].totalProfit)
+  // console.log(map.rides[0].runningCost * 16)
+}
+
+
+
+
+
+
+
+
 
 function main() {
   console.log('Initializing Tileman Plugin...');
 
+  // cheats for testing
+  context.subscribe('interval.day', function() {
+    park.cash += 1000000;
+  });
+
   if (typeof ui !== 'undefined') {
     // Register option in menu under Map icon in toolbar
-    ui.registerMenuItem('Tileman', function() { showWindow(); });
+    ui.registerMenuItem('Tileman', function() { openWindow(); });
 
-    showWindow();
+    openWindow();
+
+    // Once per second
+    context.setInterval(collectData, 1000);
+
+
   }
 }
 
@@ -85,22 +134,41 @@ registerPlugin({
   authors: ['Isoitiro'],
   type: 'remote',
   licence: 'GNU GPLv3',
-  targetApiVersion: 68,
+  targetApiVersion: 70,
+  minApiVersion: 68,
   main: main
 });
 
 /*
-  park.landPrice: 300 = 30.00
-
-
   LandSetRightsArgs
   activateTool
+  getParkStorage
 
 
 
 
 
 
+
+  subscribe(hook: "action.query", callback: (e: GameActionEventArgs) => void): IDisposable;
+  subscribe(hook: "action.execute", callback: (e: GameActionEventArgs) => void): IDisposable;
+  subscribe(hook: "interval.tick", callback: () => void): IDisposable;
+  subscribe(hook: "interval.day", callback: () => void): IDisposable;
+  subscribe(hook: "network.chat", callback: (e: NetworkChatEventArgs) => void): IDisposable;
+  subscribe(hook: "network.authenticate", callback: (e: NetworkAuthenticateEventArgs) => void): IDisposable;
+  subscribe(hook: "network.join", callback: (e: NetworkEventArgs) => void): IDisposable;
+  subscribe(hook: "network.leave", callback: (e: NetworkEventArgs) => void): IDisposable;
+  subscribe(hook: "ride.ratings.calculate", callback: (e: RideRatingsCalculateArgs) => void): IDisposable;
+  subscribe(hook: "action.location", callback: (e: ActionLocationArgs) => void): IDisposable;
+  subscribe(hook: "guest.generation", callback: (e: GuestGenerationArgs) => void): IDisposable;
+  subscribe(hook: "vehicle.crash", callback: (e: VehicleCrashArgs) => void): IDisposable;
+  subscribe(hook: "map.save", callback: () => void): IDisposable;
+  subscribe(hook: "map.change", callback: () => void): IDisposable;
+
+
+
+
+  enum textcolors
   black
   grey
   white
@@ -115,6 +183,122 @@ registerPlugin({
   lightpink
   pearlaqua
   palesilver
+
+
+
+
+
+
+
+  Ride.rideType
+  Ride.type = ' = "ride" | "stall" | "facility"
+
+  enum ridetype
+  RIDE_TYPE_SPIRAL_ROLLER_COASTER = 0,
+  RIDE_TYPE_STAND_UP_ROLLER_COASTER,
+  RIDE_TYPE_SUSPENDED_SWINGING_COASTER,
+  RIDE_TYPE_INVERTED_ROLLER_COASTER,
+  RIDE_TYPE_JUNIOR_ROLLER_COASTER,
+  RIDE_TYPE_MINIATURE_RAILWAY,
+  RIDE_TYPE_MONORAIL,
+  RIDE_TYPE_MINI_SUSPENDED_COASTER,
+  RIDE_TYPE_BOAT_HIRE,
+  RIDE_TYPE_WOODEN_WILD_MOUSE,
+  RIDE_TYPE_STEEPLECHASE = 10,
+  RIDE_TYPE_CAR_RIDE,
+  RIDE_TYPE_LAUNCHED_FREEFALL,
+  RIDE_TYPE_BOBSLEIGH_COASTER,
+  RIDE_TYPE_OBSERVATION_TOWER,
+  RIDE_TYPE_LOOPING_ROLLER_COASTER,
+  RIDE_TYPE_DINGHY_SLIDE,
+  RIDE_TYPE_MINE_TRAIN_COASTER,
+  RIDE_TYPE_CHAIRLIFT,
+  RIDE_TYPE_CORKSCREW_ROLLER_COASTER,
+  RIDE_TYPE_MAZE = 20,
+  RIDE_TYPE_SPIRAL_SLIDE,
+  RIDE_TYPE_GO_KARTS,
+  RIDE_TYPE_LOG_FLUME,
+  RIDE_TYPE_RIVER_RAPIDS,
+  RIDE_TYPE_DODGEMS,
+  RIDE_TYPE_SWINGING_SHIP,
+  RIDE_TYPE_SWINGING_INVERTER_SHIP,
+  RIDE_TYPE_FOOD_STALL,
+  RIDE_TYPE_1D,
+  RIDE_TYPE_DRINK_STALL = 30,
+  RIDE_TYPE_1F,
+  RIDE_TYPE_SHOP,
+  RIDE_TYPE_MERRY_GO_ROUND,
+  RIDE_TYPE_22,
+  RIDE_TYPE_INFORMATION_KIOSK,
+  RIDE_TYPE_TOILETS,
+  RIDE_TYPE_FERRIS_WHEEL,
+  RIDE_TYPE_MOTION_SIMULATOR,
+  RIDE_TYPE_3D_CINEMA,
+  RIDE_TYPE_TOP_SPIN = 40,
+  RIDE_TYPE_SPACE_RINGS,
+  RIDE_TYPE_REVERSE_FREEFALL_COASTER,
+  RIDE_TYPE_LIFT,
+  RIDE_TYPE_VERTICAL_DROP_ROLLER_COASTER,
+  RIDE_TYPE_CASH_MACHINE,
+  RIDE_TYPE_TWIST,
+  RIDE_TYPE_HAUNTED_HOUSE,
+  RIDE_TYPE_FIRST_AID,
+  RIDE_TYPE_CIRCUS,
+  RIDE_TYPE_GHOST_TRAIN = 50,
+  RIDE_TYPE_TWISTER_ROLLER_COASTER,
+  RIDE_TYPE_WOODEN_ROLLER_COASTER,
+  RIDE_TYPE_SIDE_FRICTION_ROLLER_COASTER,
+  RIDE_TYPE_STEEL_WILD_MOUSE,
+  RIDE_TYPE_MULTI_DIMENSION_ROLLER_COASTER,
+  RIDE_TYPE_MULTI_DIMENSION_ROLLER_COASTER_ALT,
+  RIDE_TYPE_FLYING_ROLLER_COASTER,
+  RIDE_TYPE_FLYING_ROLLER_COASTER_ALT,
+  RIDE_TYPE_VIRGINIA_REEL,
+  RIDE_TYPE_SPLASH_BOATS = 60,
+  RIDE_TYPE_MINI_HELICOPTERS,
+  RIDE_TYPE_LAY_DOWN_ROLLER_COASTER,
+  RIDE_TYPE_SUSPENDED_MONORAIL,
+  RIDE_TYPE_LAY_DOWN_ROLLER_COASTER_ALT,
+  RIDE_TYPE_REVERSER_ROLLER_COASTER,
+  RIDE_TYPE_HEARTLINE_TWISTER_COASTER,
+  RIDE_TYPE_MINI_GOLF,
+  RIDE_TYPE_GIGA_COASTER,
+  RIDE_TYPE_ROTO_DROP,
+  RIDE_TYPE_FLYING_SAUCERS = 70,
+  RIDE_TYPE_CROOKED_HOUSE,
+  RIDE_TYPE_MONORAIL_CYCLES,
+  RIDE_TYPE_COMPACT_INVERTED_COASTER,
+  RIDE_TYPE_WATER_COASTER,
+  RIDE_TYPE_AIR_POWERED_VERTICAL_COASTER,
+  RIDE_TYPE_INVERTED_HAIRPIN_COASTER,
+  RIDE_TYPE_MAGIC_CARPET,
+  RIDE_TYPE_SUBMARINE_RIDE,
+  RIDE_TYPE_RIVER_RAFTS,
+  RIDE_TYPE_50 = 80,
+  RIDE_TYPE_ENTERPRISE,
+  RIDE_TYPE_52,
+  RIDE_TYPE_53,
+  RIDE_TYPE_54,
+  RIDE_TYPE_55,
+  RIDE_TYPE_INVERTED_IMPULSE_COASTER,
+  RIDE_TYPE_MINI_ROLLER_COASTER,
+  RIDE_TYPE_MINE_RIDE,
+  RIDE_TYPE_59,
+  RIDE_TYPE_LIM_LAUNCHED_ROLLER_COASTER = 90,
+  RIDE_TYPE_HYPERCOASTER,
+  RIDE_TYPE_HYPER_TWISTER,
+  RIDE_TYPE_MONSTER_TRUCKS,
+  RIDE_TYPE_SPINNING_WILD_MOUSE,
+  RIDE_TYPE_CLASSIC_MINI_ROLLER_COASTER,
+  RIDE_TYPE_HYBRID_COASTER,
+  RIDE_TYPE_SINGLE_RAIL_ROLLER_COASTER,
+  RIDE_TYPE_ALPINE_COASTER,
+  RIDE_TYPE_CLASSIC_WOODEN_ROLLER_COASTER,
+  RIDE_TYPE_CLASSIC_STAND_UP_ROLLER_COASTER,
+
+  RIDE_TYPE_COUNT
+
+
 */
 
 
