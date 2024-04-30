89da41c2f5ff0203d964c397f386495148f3c711
Isoitiro <19146447+Haruko@users.noreply.github.com>
2024-04-29 20:19:24 -0400

    rollup config updates
    * Generalized OpenRCT2 plugin directory location (for Windows, anyway)
    * Added version number from package.json to rollup configs.

--- rollup.config.develop.js
+++ rollup.config.develop.js
@@ -1,16 +1,19 @@
 // https://github.com/Sadret/openrct2-rctris/blob/main/rollup.config.develop.js
 
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+import { homedir } from 'os';
+import npmPackage from './package.json' assert { type: 'json' };
 
 export default {
-	input: "./src/index.ts",
+	input: './src/index.ts',
 	output: [{
-		format: "iife",
-		file: "./build/openrct2-tileman-develop.js",
+		format: 'iife',
+		file: `./build/openrct2-tileman-${npmPackage.version}-develop.js`,
 	}, {
-		format: "iife",
-		file: "C:/Users/zoeyb/Documents/OpenRCT2/plugin/openrct2-tileman-develop.js",
+		format: 'iife',
+		file: `${homedir()}/Documents/OpenRCT2/plugin/openrct2-tileman-${npmPackage.version}-develop.js`,
 	}],
 	plugins: [
 		resolve(),
--- rollup.config.release.js
+++ rollup.config.release.js
@@ -1,30 +1,33 @@
 // https://github.com/Sadret/openrct2-rctris/blob/main/rollup.config.release.js
 
-import resolve from "@rollup/plugin-node-resolve";
-import terser from "@rollup/plugin-terser";
-import typescript from "@rollup/plugin-typescript";
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+
+import { homedir } from 'os';
+import npmPackage from './package.json' assert { type: 'json' };
 
 export default {
-	input: "./src/index.ts",
+	input: './src/index.ts',
 	output: [{
-		format: "iife",
-		file: "./build/openrct2-tileman-0.0.1.js",
+		format: 'iife',
+		file: `./build/openrct2-tileman-${npmPackage.version}.js`,
 	}, {
-		format: "iife",
-		file: "C:/Users/zoeyb/Documents/OpenRCT2/plugin/openrct2-tileman-0.0.1.js",
+		format: 'iife',
+		file: `${homedir()}/Documents/OpenRCT2/plugin/openrct2-tileman-${npmPackage.version}.js`,
 	}],
 	plugins: [
 		resolve(),
 		typescript(),
 		terser({
 			format: {
-				preamble: "\
+				preamble: '\
 // Copyright (c) 2024 Isoitiro\n\
 // This software is licensed under the GNU General Public License version 3.\n\
 // This software uses OpenRCT2-FlexUI by Basssiiie which is licensed under the MIT License.\n\
 // This software uses code from openrct2-rctris by Sadret which is licensed under the GNU GPLv3 License.\n\
 // The full text of these licenses can be found here: https://github.com/Haruko/OpenRCT2-Tileman\
-				",
+				',
 			},
 		}),
 	],
