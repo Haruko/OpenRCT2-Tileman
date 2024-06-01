c1a616357100b859fd39f9a6928da326f29e7729
Isoitiro <19146447+Haruko@users.noreply.github.com>
2024-06-01 03:49:08 -0400

    Who tf uses tabs

--- rollup.config.develop.js
+++ rollup.config.develop.js
@@ -11,19 +11,19 @@ import { homedir } from 'os';
 import npmPackage from './package.json' assert { type: 'json' };
 
 export default {
-	input: './src/index.ts',
-	output: [{
-		format: 'iife',
-		file: `./build/openrct2-tileman-${npmPackage.version}-develop.js`,
-	}, {
-		format: 'iife',
-		file: `${homedir()}/Documents/OpenRCT2/plugin/openrct2-tileman-${npmPackage.version}-develop.js`,
-	}],
-	plugins: [
-		inject({
-			Promise: [path.resolve('src/polyfills/promisePolyfill.ts'), 'PromisePolyfill'],
-		}),
-		resolve(),
-		typescript()
-	],
+  input: './src/index.ts',
+  output: [{
+    format: 'iife',
+    file: `./build/openrct2-tileman-${npmPackage.version}-develop.js`,
+  }, {
+    format: 'iife',
+    file: `${homedir()}/Documents/OpenRCT2/plugin/openrct2-tileman-${npmPackage.version}-develop.js`,
+  }],
+  plugins: [
+    inject({
+      Promise: [path.resolve('src/polyfills/promisePolyfill.ts'), 'PromisePolyfill'],
+    }),
+    resolve(),
+    typescript()
+  ],
 };
--- rollup.config.release.js
+++ rollup.config.release.js
@@ -12,31 +12,31 @@ import { homedir } from 'os';
 import npmPackage from './package.json' assert { type: 'json' };
 
 export default {
-	input: './src/index.ts',
-	output: [{
-		format: 'iife',
-		file: `./build/openrct2-tileman-${npmPackage.version}.js`,
-	}, {
-		format: 'iife',
-		file: `${homedir()}/Documents/OpenRCT2/plugin/openrct2-tileman-${npmPackage.version}.js`,
-	}],
-	plugins: [
-		inject({
-			Promise: [path.resolve('src/polyfills/promisePolyfill.ts'), 'PromisePolyfill'],
-		}),
-		resolve(),
-		typescript(),
-		terser({
-			format: {
-				preamble: '\
+  input: './src/index.ts',
+  output: [{
+    format: 'iife',
+    file: `./build/openrct2-tileman-${npmPackage.version}.js`,
+  }, {
+    format: 'iife',
+    file: `${homedir()}/Documents/OpenRCT2/plugin/openrct2-tileman-${npmPackage.version}.js`,
+  }],
+  plugins: [
+    inject({
+      Promise: [path.resolve('src/polyfills/promisePolyfill.ts'), 'PromisePolyfill'],
+    }),
+    resolve(),
+    typescript(),
+    terser({
+      format: {
+        preamble: '\
 // Copyright (c) 2024 Isoitiro\n\
 // This software is licensed under the GNU General Public License version 3.\n\
 // This software uses OpenRCT2-FlexUI by Basssiiie which is licensed under the MIT License.\n\
 // This software uses code from openrct2-rctris by Sadret which is licensed under the GNU GPLv3 License.\n\
 // This software uses code from OpenRCT2-Simple-Typescript-Template-Async by ItsSmitty which is licensed under the MIT License.\n\
 // The full text of these licenses can be found here: https://github.com/Haruko/OpenRCT2-Tileman\
-				',
-			},
-		}),
-	],
+        ',
+      },
+    }),
+  ],
 };
