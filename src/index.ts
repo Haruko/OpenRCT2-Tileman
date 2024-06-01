a55df757de19d95e3839d0aadb6a4d2644917ad9
Isoitiro <19146447+Haruko@users.noreply.github.com>
2024-05-31 21:12:19 -0400

    Update index.ts
    * Converted TODO comments for Todo Tree extension

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,21 @@ import { configWindow, statsWindow, toolbarWindow } from './ui';
 import { LandOwnership, getMapEdges, setLandOwnership } from './land';
 import { clearPark } from './park';
 
-/**
- * TODO: Config Window - Display spinners for each exp type
- * TODO: Config Window - Display totals for each exp type
- * TODO: Detailed Stats Window - Create window with various stats including historical data about rides done in a list view
- * TODO: Persistent Storage - context.sharedStorage for pluginconfig
- * 
- * TODO MAYBE: Data - Initialize data for constructed rides?
- * TODO MAYBE: Options - Difficulty multiplier for ParkFlags?
- * TODO MAYBE: Options - Bonus exp/tiles for completing objective?
- * TODO MAYBE: Toolbar Window - Add label over progress bar
- * TODO MAYBE: Toolbar Window - Update the tool UI to be like the land editing tool
- *   Check out https://github.com/OpenRCT2/OpenRCT2/blob/17920b60390aa0c4afc84c09aa897a596f41705a/src/openrct2-ui/windows/Land.cpp#L43
- */
+//TODO: Make tool.ts a class
+//TODO: Make some stores into computed
+//TODO: Config Window - Display spinners for each exp type
+//TODO: Config Window - Display totals for each exp type
+//TODO: Detailed Stats Window - Create window with various stats including historical data about rides done in a list view
+//TODO: Persistent Storage - context.sharedStorage for pluginconfig
+
+//TODO: MAYBE: Data - Initialize data for constructed rides?
+//TODO: MAYBE: Options - Difficulty multiplier for ParkFlags?
+//TODO: MAYBE: Options - Bonus exp/tiles for completing objective?
+//TODO: MAYBE: Toolbar Window - Add label over progress bar
+//TODO: MAYBE: Toolbar Window - Update the tool UI to be like the land editing tool
+//TODO:     Check out https://github.com/OpenRCT2/OpenRCT2/blob/17920b60390aa0c4afc84c09aa897a596f41705a/src/openrct2-ui/windows/Land.cpp#L43
+
+
 
 
 
