4a6ef9f8c0bdc7cadf9788f1dc604f83ac43b784
Isoitiro <19146447+Haruko@users.noreply.github.com>
2024-06-29 20:11:03 -0400

    Added Templater class

---
+++ src/ui/Templater.ts
@@ -0,0 +1,48 @@
+import { ElementID } from './types/enums';
+import { UIElement } from './types/types';
+
+export abstract class Templater<T> {
+  private readonly _uiElementMap : Partial<Record<ElementID, UIElement>> = {};
+  protected template! : T;
+
+  constructor() {
+    this.template = this._buildTemplate();
+  }
+
+  /**
+   * Builds the template for initialization
+   * @returns Template of type T
+   */
+  protected abstract _buildTemplate() : T;
+
+  /**
+   * Retrieves the template
+   */
+  public getTemplate() : T {
+    return this.template;
+  }
+
+  /**
+   * Registers a new element to the list of elements
+   * @param id ID to register to
+   * @param el Element to register
+   * @returns True if the instance was set, otherwise false 
+   */
+  protected registerElement(id : ElementID, el : UIElement) : boolean {
+    if (typeof this._uiElementMap[id] === 'undefined') {
+      this._uiElementMap[id] = el;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Gets a UI element from the map
+   * @param id ElementID to get instance of
+   * @returns Button instance
+   */
+  public getChildElement(id : ElementID) : UIElement | undefined {
+    return this._uiElementMap[id];
+  }
+}
