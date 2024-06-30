18b1241483b2d2324f3b6908cdb2d73f5d0fa135
Isoitiro <19146447+Haruko@users.noreply.github.com>
2024-06-29 20:11:15 -0400

    Added BaseTab class

---
+++ src/ui/windows/tabs/BaseTab.ts
@@ -0,0 +1,44 @@
+/// <reference path='../../../../lib/openrct2.d.ts' />
+
+import { IWindow } from '../IWindow';
+import { TabCreator } from 'openrct2-flexui';
+import { Templater } from '@src/ui/Templater';
+
+export abstract class BaseTab extends Templater<TabCreator> {
+  readonly parent : IWindow;
+
+  protected constructor(parent : IWindow) {
+    super();
+    this.parent = parent;
+  }
+  
+  
+  
+
+  /**
+   * **********
+   * Event Handling
+   * **********
+   */
+
+  /**
+   * Handles onOpen event
+   */
+  protected onOpen() : void {
+
+  }
+
+  /**
+   * Handles onUpdate event
+   */
+  protected onUpdate() : void {
+
+  }
+
+  /**
+   * Handles onClose event
+   */
+  protected onClose() : void {
+    
+  }
+}
