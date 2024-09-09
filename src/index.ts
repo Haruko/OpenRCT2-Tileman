c5080cc085d75b3ba58c9cc7938c4f4404ff7aca
Isoitiro <19146447+Haruko@users.noreply.github.com>
2024-09-09 00:16:24 -0400

    Updated to v1.2.1

--- src/index.ts
+++ src/index.ts
@@ -5,55 +5,6 @@ import { loadPark } from './pluginInitializer';
 import { StartupWindow } from './ui/windows/StartupWindow';
 
 
-/*
-
-* A lot of background stuff you won't see, but causes previous saves to break (sorry)
-* Updated initial XP values (Can be changed in config window)
-* Added XP sources:
-  - Balloons popped
-  - Banners placed
-    - XP is removed when banner is removed
-  - Park awards
-    - Positive
-    - Negative
-  - Marketing campaigns
-    - Per $50 spent
-  - Scenario completion
-  - Vehicle crashes
-    - Per car exploded
-    - Per guest killed
-  - Guests and staff drowning
-* New restrictions on tile ownership changes:
-  * Guests - Selling - No longer allowed to sell tiles with guests on them.
-  * Paths - Buying - No longer allowed to buy tiles with paths on them. (Can be bypassed in config window)
-  * Paths - Rights - No longer allowed to switch between owned and construction rights owned. Construction rights CAN be bought on land that is unowned. (Can be bypassed in config window)
-  * Paths - Selling - No longer allowed to sell tiles with paths on them.
-* New startup process:
-  * Added popup window on load and now pause the game to ask if you want to play tileman or not.
-  * All paths within the park are now cleared.
-  * Guest clearing is limited to within the park.
-* Toolbar Window:
-  * Made toolbar smaller by removing config and stats buttons. These are both accessible from the map dropdown menu.
-* Config Window:
-  * Made it look nicer.
-  * Added count of instances for each XP source.
-  * Added tooltips to config window and cleaned it up a bit.
-  * Added bypass for path restrictions in config.
-  * Added button to delete all owned paths (and clean up litter on them).
-  * Added tooltips to buttons in config and debug tabs to tell people to double click.
-
-
-
-
-
-
-
-
-
-
-*/
-
-
 
 
 
@@ -89,15 +40,20 @@ import { StartupWindow } from './ui/windows/StartupWindow';
     //       42.64 [8] Imp, Cow
 
 // LOW PRIORITY
-  //TODO: Config Window - Add reset park data button
   //TODO: Detailed Stats Window - Create window with various stats including historical data about rides done in a list view
 
 // UNSURE
+  //TODO: Config Window - Add reset park data button
   //TODO: Persistent Storage - context.sharedStorage for plugin config with profiles?
   //TODO: XP Sources - Make a baseline for each scenario type and multiple xp value by how much over the baseline it is (200 vs 400 guests = 2x)
   //TODO: Options - Difficulty multiplier for ParkFlags?
-  //TODO: Startup Behavior - Don't clear rides (but still clear guests and staff) if Abandoned Park plugin is detected
+  //TODO: Startup Behavior - 
+
+// Integrations
+  // Abandoned Park - Don't clear rides (but still clear guests and staff) if Abandoned Park plugin is detected
     // https://openrct2plugins.org/plugin/MDEwOlJlcG9zaXRvcnkzNDM0NTI2NjY=/openrct2-abandonedpark
+  // Animated Vandalism Repairing - Add path element repairs as an XP Source
+    // https://github.com/jpknen/openrct2-animated-vandalism-repairing
 
 
 
